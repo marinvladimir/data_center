6e8e303614c006a50b35e0bfd29e4d40f941bec3
unknown <mvladi00@fesb.hr>
2019-09-23 19:09:03 +0200

    Updates

---
+++ precache-manifest.cbdb058bd89356e19dcf5fca9448c45e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69710bbbbd50984ab137dfdfb03a61c5",
+    "url": "/data_center/index.html"
+  },
+  {
+    "revision": "c0daf82959c03d5a2270",
+    "url": "/data_center/static/css/main.1ff91385.chunk.css"
+  },
+  {
+    "revision": "1b0e25905a9ca75beacd",
+    "url": "/data_center/static/js/2.db20c433.chunk.js"
+  },
+  {
+    "revision": "c0daf82959c03d5a2270",
+    "url": "/data_center/static/js/main.ad6ef75f.chunk.js"
+  },
+  {
+    "revision": "0bbb423c6803a2e18e68",
+    "url": "/data_center/static/js/runtime~main.1e995a01.js"
+  },
+  {
+    "revision": "4ebecaf99d1422c1a396172fb18cc390",
+    "url": "/data_center/static/media/Cooler_Panel_17_9.4ebecaf9.txt"
+  },
+  {
+    "revision": "b86685740b650b12b07bdacd96ecbe94",
+    "url": "/data_center/static/media/Cooler_Stats_17_9.b8668574.txt"
+  },
+  {
+    "revision": "6f1660ea930bd7bf386d59300c0c2302",
+    "url": "/data_center/static/media/Generator_Monitor.6f1660ea.txt"
+  },
+  {
+    "revision": "6346609e1b7abfe2ddbe1123dc70d119",
+    "url": "/data_center/static/media/Machine-1_info_ram_10_9.6346609e.txt"
+  },
+  {
+    "revision": "aff2a152f63f426c9305074e5e0749ce",
+    "url": "/data_center/static/media/Machine-2_info_ram_11_9.aff2a152.txt"
+  },
+  {
+    "revision": "312817a693598f3e76c935a46523444a",
+    "url": "/data_center/static/media/Machine_1_info_10_9.312817a6.txt"
+  },
+  {
+    "revision": "83fd3253bbc103daaeef55560c44ba62",
+    "url": "/data_center/static/media/Machine_1_info_df_10_9.83fd3253.txt"
+  },
+  {
+    "revision": "bbb11073cc178affebec5f844f3aec22",
+    "url": "/data_center/static/media/Machine_2_info_11_9.bbb11073.txt"
+  },
+  {
+    "revision": "9df2f804fd31d3872d18a28ae490b632",
+    "url": "/data_center/static/media/Machine_3_info_17_9.9df2f804.txt"
+  },
+  {
+    "revision": "e9f4c2209b7c01b5a4b3ae811d40a902",
+    "url": "/data_center/static/media/Machine_3_info_17_9_2.e9f4c220.txt"
+  },
+  {
+    "revision": "a88f9413daf6b960dd271f285c587b31",
+    "url": "/data_center/static/media/Machine_3_info_df_17_9.a88f9413.txt"
+  },
+  {
+    "revision": "da4b6163c26c03f43f1137e04604db19",
+    "url": "/data_center/static/media/Machine_3_info_ram_17_9.da4b6163.txt"
+  },
+  {
+    "revision": "179903dd2c84db1506928436f28565e3",
+    "url": "/data_center/static/media/Page_Defrag-logo.179903dd.png"
+  },
+  {
+    "revision": "894bbc3cdb44af1cc3a709109eea9f4e",
+    "url": "/data_center/static/media/RestAPI.894bbc3c.txt"
+  },
+  {
+    "revision": "b94ba9c2b23a84fc8fcc4a783698da1a",
+    "url": "/data_center/static/media/graph.b94ba9c2.png"
+  },
+  {
+    "revision": "80ed98f23e901d6524dd0b0aff2c095f",
+    "url": "/data_center/static/media/host.80ed98f2.png"
+  }
+]);
